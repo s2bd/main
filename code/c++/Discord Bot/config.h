22d4d36ca8466d5bd0ea44a791ae594946c846ba
Dewan Mukto <dewanmukto@gmail.com>
2025-03-18 15:23:30 +0600

    Asset updates

--- code/c++/Discord Bot/config.h
+++ code/c++/Discord Bot/config.h
@@ -17,14 +17,14 @@ struct Config {
 
 Config loadConfig() {
     Config config;
-    config.token = "OTc3MjYyOTUyMzU0ODI4MzA4.GVV0Qu.OadGqegq7EbKaHp6W1F-EZAZQISh9IayDPwqsE"; // Replace with your token
+    config.token = "";
     config.prefix = "!"; // Replace with your desired prefix
     config.leaveIdle = true;
     config.leaveIdleCountdown = 60;
     config.leaveFinish = false;
     config.leaveStop = false;
     config.webhookId = 980158201297248257;
-    config.webhookToken = "vMjZOZY4R2Czxaxp2w9Dlzuudnnt90Srm1nlFsxZyMual96s9W3MFFFhtn3tU0mMgUQJ"; // Replace with your webhook token
+    config.webhookToken = ""; // Replace with your webhook token
     return config;
 }
 
