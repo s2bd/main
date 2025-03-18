de0c69d4dd811f7c1d17f7adcd2dd52c2a002e06
Dewan Mukto <dewanmukto@gmail.com>
2025-03-18 15:54:31 +0600

    temporary

--- code/java/InventoryApp/InventoryApp.java
+++
@@ -1,6 +0,0 @@
-public class InventoryApp {
-    public static void main(String[] args) {
-        InventoryForm form = new InventoryForm();
-        form.setVisible(true);  // Show the form
-    }
-}
--- code/java/MandelEx/Settings.java
+++
@@ -1,5 +0,0 @@
-public class Settings {
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 800;
-    public static final int MAX_ITERATIONS_BASE = 1000;
-}
